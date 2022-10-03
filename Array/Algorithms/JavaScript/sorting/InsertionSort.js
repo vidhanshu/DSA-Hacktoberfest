069adbaf9c951c035143561b4e09c8f96753a742
Prathamesh Naik <cspcce2023@gmail.com>
2022-10-03 12:15:08 +0530

    Added Insertion Sort

---
+++ Array/Algorithms/JavaScript/sorting/InsertionSort.js
@@ -0,0 +1,18 @@
+
+const insertionSort = arr => {
+    const len = arr.length;
+    for (let i = 0; i < len; i++) {
+      let el = arr[i];
+      let j;
+  
+      for (j = i - 1; j >= 0 && arr[j] > el; j--) {
+        arr[j + 1] = arr[j];
+      }
+      arr[j + 1] = el;
+    }
+    return arr;
+  };
+
+
+  const arr=[55,67,1,99,75,89]
+  console.log(insertionSort(arr))
