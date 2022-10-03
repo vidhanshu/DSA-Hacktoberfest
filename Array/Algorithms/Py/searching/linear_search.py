d4a7baa070abe170b6a667fb211d06623a1061fa
pratik swami <pratikswami4@gmail.com>
2022-10-03 11:02:47 +0530

    added linear_search in python #47

---
+++ Array/Algorithms/Py/searching/linear_search.py
@@ -0,0 +1,21 @@
+# Implementation of counting sort in Python3
+
+def linear_search(arr, target):
+    i=0
+    n = len(arr)
+    while i<n:
+        if target == arr[i]:
+            return i
+        i+=1
+    if i==n:
+        return -1
+    
+if __name__ == "__main__":
+    print("Enter the array elements :")
+    arr = [int(x) for x in input().split(" ")] # Takes Input from the user
+    target = int(input("Enter target element: "))
+    index = linear_search(arr, target)
+    if index == -1:
+        print(f"{target} not found.")
+    else:
+        print("{} found at index {}".format(target, index))
